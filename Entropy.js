863903f00f0cf6c9311cd58a230cae8a1249adac
Andrey Shchegolev <91487145+Goodpurpp@users.noreply.github.com>
2021-09-29 23:47:55 +0500

    Update Entropy.js

--- Entropy.js
+++ Entropy.js
@@ -7,20 +7,25 @@ let Entropy = 0;//Переменная где мы будем содержать
 inputData = inputData.toString();// преобразование байтового буфера в строку
 for (let i = 0; i < inputData.length; i++) {// Инициализация массива Alph
     Alph[inputData.charAt(i)] = 0;
+
 }
 //Результатом инициализации ключами в массива alph являются все различные массивы алфавита, со значениями 0
 //Считаем число повторов букв
-let counter = 0;
 for (let i = 0; i < inputData.length; i++) {
     Alph[inputData.charAt(i)] += 1;
-    counter += 1;
 }
 //todo: прочитать и понять,почему длинна массива Alph равна нулю.Длина определяется последним числовым индексом
 /*for (let i=0;i<Alph.length;i++){
     Alph[i]/=inputData.length;
 }*/ //Будет работать но неправильно <---
+let counter = 0;
 for (i in Alph) {
     Alph[i] /= inputData.length;
+    counter++;
+}
+if (counter == 1) {
+    console.log(0);
+    return 0;
 }
 for (i in Alph) {
     Entropy -= Alph[i] * (Math.log(Alph[i]) / Math.log(counter));
